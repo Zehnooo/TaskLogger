8c80c0b140720cf2241f9e33028399121d05f2ed
Zehnooo <126801501+Zehnooo@users.noreply.github.com>
2025-06-17 16:23:20 -0500

    Grabbing inputs via js

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 
 const form = document.getElementById('task-form');
-const customRequest = document.getElementById('custom-request');
 
-// inputs
+// BEGIN form inputs
+const customRequest = document.getElementById('custom-request');
 const taskNameInput = document.getElementById('task-name');
 const taskDescInput = document.getElementById('task-desc');
 const taskDepartmentInput = document.getElementById('task-department');
-const selectedCategory = document.getElementById('task-category');
+const selectedCategoryInput = document.getElementById('task-category');
+// END inputs
 
+// BEGIN grab elements
+const activeTask = document.getElementById('active-task');
 
+// BEGIN custom request function
 function customInputShow(){
-console.log(selectedCategory.value);
-if (selectedCategory.value === "Custom Request"){
+console.log(selectedCategoryInput.value);
+if (selectedCategoryInput.value === "Custom Request"){
 customRequest.style.display = 'flex';
 } else {
     customRequest.style.display = 'none';
 }
 }
+// End custom request function
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
     const taskName = taskNameInput.value.trim();
+    const taskDesc = taskDescInput.value.trim();
+    const taskDepartment = taskDepartmentInput.value.trim();
+    const taskCategory = selectedCategoryInput.value.trim();
     console.log("Task Name is: ", taskName);
+    console.log("Task Description is: ", taskDesc);
+    console.log("Task Department is: ", taskDepartment);
+    console.log("Task Category is: ", taskCategory);
+    activeTask.classList.remove('hidden');
 });
